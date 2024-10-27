d01de3380630816c2fb7274c1fb40522064023bf
Paul Gush <Paul1208Gush@Gmail.com>
2024-10-27 15:02:57 +0000

    Refactoring/Optimization, removed debug

--- Assets/Scripts/Connection.cs
+++ Assets/Scripts/Connection.cs
@@ -19,8 +19,6 @@ namespace RedEngine
             m_positions[3].position = target.position;
             m_positions[3].SetParent(target, true);
             m_positions[3].Translate(0,0,1);
-            
-            Debug.Log(origin.position + ", " + GetWorldPositionFromDistance(origin, target, 0.5f));
         }
 
         public void SetArcColor(Color newColor)
@@ -42,9 +40,7 @@ namespace RedEngine
 
         private Vector3 GetWorldPositionFromDistance(Transform origin, Transform target, float percentage)
         {
-            Vector3 newPosition = Vector3.Lerp(origin.position, target.position, percentage);
-            
-            return newPosition;
+            return Vector3.Lerp(origin.position, target.position, percentage);
         }
     }
 }
